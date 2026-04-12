93ed63c821c57e1540b6d8024f13a764a7c3428f
bobas17 <bobascharles@gmail.com>
2026-04-12 21:54:46 +0530

    Update settings.py

--- spidernet/spidernet/settings.py
+++ spidernet/spidernet/settings.py
@@ -121,3 +121,20 @@ STATIC_URL = 'static/'
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if os.environ.get('DJANGO_SUPERUSER_USERNAME'):
+    if not User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).exists():
+        User.objects.create_superuser(
+            username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+            email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+            password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        )
